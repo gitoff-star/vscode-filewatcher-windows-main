6da6d9799177933faa6e04371e52c7e531e9ec5b
gitoff-start <replyban@gmail.com>
2023-07-24 09:44:26 +0500

    setting up connection

---
+++ FileWatcher/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    class Class1
+    {
+        public Class1(String Connection)
+        {
+                
+        }
+
+        public int isConnected { get; set; }
+    }
+}
